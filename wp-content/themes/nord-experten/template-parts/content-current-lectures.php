6faacfb84cff625bc6141e945f239fb780fbc586
fifi2na <filipmalecki94@gmail.com>
2022-01-20 22:55:50 +0100

    fix current lectures showing

--- wp-content/themes/nord-experten/template-parts/content-current-lectures.php
+++ wp-content/themes/nord-experten/template-parts/content-current-lectures.php
@@ -1,5 +1,3 @@
-<?php if( !current_user_can('administrator') && !current_user_can('contributor')) : ?>
-    <div id="current-lectures">
-        <?php echo do_shortcode('[events_list limit="3" category="Vortrag"]'); ?>
-    </div>
-<?php endif; ?>
+<div id="current-lectures">
+    <?php echo do_shortcode('[events_list limit="3" category="Treffen"]'); ?>
+</div>
